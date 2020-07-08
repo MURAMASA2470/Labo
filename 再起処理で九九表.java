87ad06567f1c725b70c78a6e890316d2c4d7eaea
Kimura Keisuke <keisuke226214@gmail.com>
2020-07-09 01:37:59 +0900

    Create 再起処理で九九表.java

---
+++ 再起処理で九九表.java
@@ -0,0 +1,21 @@
+public class Sample {
+
+	public static void main(String[] args) {
+		// TimesTables(行数, 列数)
+		TimesTables(9, 9);
+	}
+	
+	static int Multiply(int from, int to, int current) {
+		System.out.print(String.format("%4s", current));
+		return current < to ? Multiply(from, to, current + from) : from;
+	}
+	
+	static int TimesTables(int row, int col, int current) {
+		Multiply(current, col * current, current);
+		System.out.println("\n");
+		return current < row ? TimesTables(row , col, ++current) : -1;
+	}
+	
+	// デフォルト引数用のオーバーロード
+	static void TimesTables(int r, int c) { TimesTables(r, c, 1); }
+}
