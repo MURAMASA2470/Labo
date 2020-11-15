d6b0e8044511620edbf697678dbb6b7c1a6cbc0b
Kimura Keisuke <keisuke226214@gmail.com>
2020-11-16 02:43:22 +0900

    Update SettingStyles2.vb

--- SettingStyles2.vb
+++ SettingStyles2.vb
@@ -13,41 +13,80 @@ Sub WorkbookXXX()
         End If
     End With
     
-    
     Set fso = CreateObject("Scripting.FileSystemObject")
-    Set Files = fso.GetFolder(folderPath).Files
+    Set folderObj = fso.GetFolder(folderPath)
+    Set files = folderObj.files
+    Set SubFolders = folderObj.SubFolders
+    
+    Call ProcessFolder(files)
+    Call ProcessFolder(SubFolders)
+    
+    Set fso = Nothing
+        
+    Application.ScreenUpdating = True
+    
+    
+End Sub
 
-' ƒfƒBƒŒƒNƒgƒŠ‚È‚¢‚Ìƒtƒ@ƒCƒ‹‚ðŽæ“¾
 
-    For Each file In Files
+Sub ProcessFolder(ByVal files As Object)
+
+    Set fso = CreateObject("Scripting.FileSystemObject")
+
+    For Each file In files
+        
+       If fso.FileExists(file) Then
+            Call ProcessFile(file)
+        ElseIf fso.FolderExists(file) Then
+            Set folderObj = fso.GetFolder(file)
+            Set inFiles = folderObj.files
+            Set inSubFolders = folderObj.SubFolders
+    
+            Call ProcessFolder(inFiles)
+            Call ProcessFolder(inSubFolders)
+        End If
+    
+    Next file
+    
+    Set fso = Nothing
+
+End Sub
+
+
+Sub ProcessFile(ByVal file As Object)
+
+    ' Skipping hidden file
+    If Left(file.Name, 2) = "~$" Then
+        GoTo SKIP_LOOP
+    End If
         
         If Dir(file) = "" Then
-            MsgBox filePath & vbCrLf & "‚Í‘¶Ý‚µ‚Ä‚¢‚Ü‚¹‚ñ", vbExclamation
+            MsgBox filePath & vbCrLf & "File Not Found", vbExclamation
             Exit Sub
         End If
         For Each wb In Workbooks
             If wb.Name = buf Then
-                MsgBox filePath & vbCrLf & "‚Í‚·‚Å‚ÉŠJ‚©‚ê‚Ä‚¢‚Ü‚·", vbExclamation
+                MsgBox filePath & vbCrLf & "File Opened", vbExclamation
                 Exit Sub
             End If
         Next wb
     
-        Workbooks.Open file
+        Set currentWorkbook = Workbooks.Open(Filename:=file)
         
         'currentWorkbook.Activate
         'ActiveWindow.Visible = False
         
         'For i = 1 To currentWorkbook.Worksheets.Count
         
-        For Each currentWorkSheet In ActiveWorkbook.Worksheets
+        For Each currentWorkSheet In currentWorkbook.Worksheets
         
-            currentWorkSheet.Activate
+            'currentWorkSheet.Activate
             
              With currentWorkSheet.PageSetup
-                .Orientation = xlPortrait 'ˆóüŒü‚«‚ðc•ûŒü‚ÉÝ’è
-                .Zoom = False 'Šg‘åk¬‚ðÝ’èi‚µ‚È‚¢j
-                .FitToPagesWide = 1 '‚·‚×‚Ä‚Ì—ñ‚ð1ƒy[ƒW‚Éˆóü
-                .FitToPagesTall = False 'ƒV[ƒg‚ð1ƒy[ƒW‚Éˆóü
+                .Orientation = xlPortrait '?o?u?u?á?d?c?u?u?E?Yfe
+                .Zoom = False '?gea?k?Ê?d?Yfe?i?ƒÊ?E?‘?j
+                .FitToPagesWide = 1 '?E?~?A?I?n?d1?y?[?W?E?o?u
+                .FitToPagesTall = False '?V?[?g?d1?y?[?W?E?o?u
             End With
         
         Next
@@ -57,12 +96,8 @@ Sub WorkbookXXX()
         currentWorkbook.Save
         
         currentWorkbook.Close
-    
-    Next file
-    
-    Set fso = Nothing
         
-    Application.ScreenUpdating = True
-    
-    
+        
+SKIP_LOOP:
+
 End Sub
